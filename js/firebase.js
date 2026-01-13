a45ff207080eb6d02c3b6fb93e1cb37732f85f5b
Love creation <lovecreationinfo@gmail.com>
2026-01-13 14:39:39 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -6,32 +6,14 @@
 
 <script>
   // Your Firebase configuration
-  <!-- Firebase SDKs -->
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-storage-compat.js"></script>
-
-<script>
-  // Your Firebase configuration
   const firebaseConfig = {
-    apiKey: "PASTE_HERE",
-    authDomain: "PASTE_HERE",
-    projectId: "PASTE_HERE",
-    storageBucket: "PASTE_HERE",
-    messagingSenderId: "PASTE_HERE",
-    appId: "PASTE_HERE"
-  };
-
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-  // Firebase services
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-  const storage = firebase.storage();
-</script>
-
+  apiKey: "AIzaSyDh7U6z4P_leos-l4MC-tHuftlItwSHAHQ",
+  authDomain: "lovecreationwebsite.firebaseapp.com",
+  projectId: "lovecreationwebsite",
+  storageBucket: "lovecreationwebsite.firebasestorage.app",
+  messagingSenderId: "973080872272",
+  appId: "1:973080872272:web:587db074ab95e328e05855"
+};
 
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
